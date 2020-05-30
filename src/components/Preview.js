8d697a56e57a72d3922924158bec4050fe0608a4
Diego Lopez <lopexdiego5@gmail.com>
2020-05-30 01:22:46 -0500

    added styles

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -2,22 +2,17 @@ import React from 'react';
 
 //!markdown library
 import marked from 'marked';
+
 marked.setOptions({
     breaks: true,
 });
 
-// INSERTS target="_blank" INTO HREF TAGS (required for codepen links)
 const renderer = new marked.Renderer();
-// renderer.link = function (href, title, text) {
-// return `<a target="_blank" href="${href}">${text}` + '</a>';
-// }
 
 class Preview extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            // markedOutout: marked('# hola')
-        }
+        this.state = {}
     }
     render() {
 
@@ -28,9 +23,3 @@ class Preview extends React.Component {
 }
 
 export default Preview;
-
-// const Preview = (props) => {
-//     return (
-//         <div id='preview' dangerouslySetInnerHTML={{__html: marked(props.markdown, { renderer: renderer })}} />
-//       )
-//   }
